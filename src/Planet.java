6b48077b4226b6b1fc39984e7709d82563fd2885
Tim <103999993+FireYSKK@users.noreply.github.com>
2022-09-18 20:25:07 +0300

    Fixed empty files

--- src/Planet.java
+++ src/Planet.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Planet {
+public class Planet implements Nameable{
+    private String name;
+
+    public Planet(String name){
+        setName(name);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/TestPlanet.java
+++ src/TestPlanet.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class TestPlanet {
+public class TestPlanet {
+    public static void main(String[] args) {
+        Nameable planet1 = new Planet("Mercury");
+        Nameable planet2 = new Planet("Earth");
+        Nameable planet3 = new Planet("Neptune");
+
+        System.out.println(planet1.getName());
+        System.out.println(planet2.getName());
+        System.out.println(planet3.getName());
+    }
 }
